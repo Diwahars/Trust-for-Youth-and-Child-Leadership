ed7825c5fb94af96b25e6b017b18a0a2483609d4
vapalanisamy <vapalanisamy@paypal.com>
2015-11-29 14:31:35 +0530

    Notification functionality added.

--- android_app/NKana/src/main/java/com/nkana/app/activity/VolunteerMainActivity.java
+++ android_app/NKana/src/main/java/com/nkana/app/activity/VolunteerMainActivity.java
@@ -1,5 +1,8 @@
 package com.nkana.app.activity;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -63,12 +66,35 @@ public class VolunteerMainActivity extends NavigationLiveo implements OnItemClic
         dbConnection = new DBConnection(mContext);
         preferences = getSharedPreferences(IConstants.AUTH_TOKEN, MODE_PRIVATE);
         authKey = preferences.getString(IConstants.AUTHORIZATION, null);
-        if (dbConnection.checkUserAvailable()) {
-            checkUserProfile();
-        } else {
-//            getProfile();
-        }
+        notification();
+//        if (dbConnection.checkUserAvailable()) {
+//            checkUserProfile();
+//        } else {
+////            getProfile();
+//        }
     }
+
+
+    private void notification(){
+        Intent intent = new Intent(this, VolunteerMainActivity.class);
+        // use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        // build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("NKana Notification")
+                .setContentText("The Traker not updated")
+                .setSmallIcon(R.mipmap.ic_stat_nkana_logo)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0,n);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
